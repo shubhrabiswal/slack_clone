27fe3ef0821253b53f08d00de22d93178be0eb69
Shubhra Biswal <shubhrabiswal08@gmail.com>
2021-07-27 21:26:42 +0530

    commit 5

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,39 +2,84 @@ import React from 'react'
 import "./Login.css";
 import { Button } from "@material-ui/core";
 import { auth, provider } from "./firebase";
+import firebase from 'firebase'
+import { auth, provider } from "./firebase.js";
 import { useStateValue } from "./StateProvider";
 import { actionTypes } from "./reducer";
+import { GoogleLogin } from 'react-google-login';
+// import Icon from './icon';
+const useStyles = () => { }
 
 const Login = () => {
+    const classes = useStyles();
     const [state, dispatch] = useStateValue();
 
     const signIn = () => {
         auth.signInWithPopup(provider)
+        // auth.signInWithPopup(provider)
+        auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
             .then((result) => {
                 dispatch({
                     type: actionTypes.SET_USER,
-                    user: result.user,
-                });
-            })
-            .catch((error) => {
+@ -20,6 +26,36 @@ const Login = () => {
                 alert(error.message);
             });
     };
+    const googleSuccess = async (res) => {
+        // window.alert("Login Successful")
+        console.log("res",res)
+        console.log(res.profileObj)
+        const result  = res?.profileObj;
+        console.log("result",result)
+        const token = res?.tokenId;
+        // console.log("token",token)
+
+        try{
+            dispatch({ 
+                type: actionTypes.SET_USER,
+                user: result,
+                // data: { result, token } 
+            })
+            // history.push("/");
+        }catch(error){
+            console.log(error);
+        }
+        
+    }
+
+    const googleFailure = (err) => {
+        console.log(err)
+        console.log("Google Sign in was unsuccessful")
+    }
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+    console.log("lsjfljlhfhlhsl")
 
     return (
         <div className="login" >
-            <div className="login__container">
-                <img
-                    src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"
+@ -29,9 +65,21 @@ const Login = () => {
                     alt=""
                 />
 
                 <h1>Sign in to Clever Programmer</h1>
                 <p>cleverprogrammer.slack.com</p>
                 <Button onClick={signIn}>Sign in with Google</Button>
+                <h1>Sign in</h1>
+                <p>sign.slack.com</p>
+                {/* <Button onClick={signIn}>Sign in with Google</Button> */}
+                <GoogleLogin
+                    clientId="162754271125-fd79e2688vimj32jdj1ah2v03vk279jc.apps.googleusercontent.com"
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                        SignIn</button>
+                      )}
+                    onSuccess={googleSuccess}
+                    onFailure={googleFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+
             </div>
         </div >
     )
-}
-
-export default Login
